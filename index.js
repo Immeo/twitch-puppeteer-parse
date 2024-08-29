cfa913a9c23c91ab8bafbac46b04058ad2634c22
immeo <wayfarer245@gmail.com>
2024-08-29 15:51:51 +0300

    03-add output title stream

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const PARSE_CHANNEL = 'name_channel';
 const checkStream = async () => {
 	let statusStreamNow = false;
 	let titleStreamNow = '';
-	const browser = await puppeteer.launch({ headless: false });
+	const browser = await puppeteer.launch({ headless: true });
 	const page = await browser.newPage();
 	await page.goto(`https://www.twitch.tv/${PARSE_CHANNEL}`, {
 		waitUntil: 'domcontentloaded'
@@ -14,6 +14,7 @@ const checkStream = async () => {
 	try {
 		await page.waitForSelector('.bfNjIO', { timeout: 15000 });
 		const checkHaveClass = await page.$('.bfNjIO', { timeout: 10000 });
+
 		if (checkHaveClass) {
 			statusStreamNow = true;
 			console.log('Element found');
@@ -21,17 +22,19 @@ const checkStream = async () => {
 			console.log(`Element ${checkHaveClass} not found`);
 			statusStreamNow = false;
 		}
+
+		const titleStream = await page.$eval(
+			'h2[data-a-target="stream-title"]',
+			el => el.innerText
+		);
+		return { statusStreamNow, titleStream };
 	} catch (error) {
-		if (error) console.log('Element not found');
+		if (error) console.log(error);
 	} finally {
-		if (statusStreamNow) {
-			titleStreamNow = await page.$eval('.bfNjIO', el => el.innerText);
-			console.log(`Stream is on now: ${PARSE_CHANNEL}`);
-		} else {
-			console.log('Stream is off now');
-		}
 		await browser.close();
 	}
 };
 
-checkStream();
+checkStream().then(res => {
+	console.log(res);
+});
